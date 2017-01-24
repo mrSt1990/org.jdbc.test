e4e280920e9d5e4b448922f2a7bf53005e4094ef
Aleksey Stoyokha <astoyoha@mail.ru>
2017-01-24 19:43:07 +0300

    Hibernate config modified

--- src/main/java/org/data/util/HibernateUtil.java
+++ src/main/java/org/data/util/HibernateUtil.java
@@ -1,7 +1,23 @@
 package org.data.util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Created by mr_St on 24.01.17.
  */
 public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+    static {
+        try {
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation falied." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
 }
